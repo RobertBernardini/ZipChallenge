c72d74e426b42317867079afbd7cbe53474e1554
Robert Bernardini <robert@tinybeans.com>
2020-02-21 17:37:06 +1100

    Initial Commit

---
+++ ZipChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZipChallenge
+//
+//  Created by Robert Bernardini on 21/2/20.
+//  Copyright © 2020 Robert Bernardini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
